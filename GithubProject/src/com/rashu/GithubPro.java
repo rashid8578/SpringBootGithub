5cbcca182a31b97b79d13b2830625d77aa881ef6
cutm <debendra.maharana@cutm.ac.in>
2024-09-16 21:49:11 +0530

    second commit

--- GithubProject/src/com/rashu/GithubPro.java
+++ GithubProject/src/com/rashu/GithubPro.java
@@ -5,6 +5,8 @@ public class GithubPro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Wel come to the GitHub");
+System.out.println("Wel come to the GitHub");
+
 	}
 
 }
