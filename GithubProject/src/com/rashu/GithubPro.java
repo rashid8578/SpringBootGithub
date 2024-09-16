3c9e0ce1cb7212a5f41ee8072921210e3dcf1f3f
cutm <debendra.maharana@cutm.ac.in>
2024-09-16 21:42:38 +0530

    this project is commit

---
+++ GithubProject/src/com/rashu/GithubPro.java
@@ -0,0 +1,10 @@
+package com.rashu;
+
+public class GithubPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wel come to the GitHub");
+	}
+
+}
